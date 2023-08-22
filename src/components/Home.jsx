dd8ad8f6b4ed2964742e3b7658d8c4594e8297ce
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2023-08-22 07:56:25 +0600

    home page left part added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,44 @@
+import { ShoppingCartIcon } from "@heroicons/react/24/solid";
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    const handleNavigate = () => {
+        navigate("/books");
+    };
+
     return (
-        <div>
-            <h2>Home</h2>
+        <div className="flex justify-between">
+            <div className="my-container">
+                <p className="bg-yellow-400 inline px-3 py-1 rounded-full text-xs font-semibold text-gray-700">
+                    ON SALE!
+                </p>
+                <h1 className="text-4xl font-bold text-gray-900 my-4">
+                    A reader lives a <br /> thousand lives{" "}
+                    <span className="text-blue-400">before he dies</span>
+                </h1>
+                <p>
+                    Books are a uniquely portable magic. Books serve to show a
+                    man that those original thoughts of his aren’t very new
+                    after all. The man who does not read good books is no better
+                    than the man who can’t.
+                </p>
+                <div className="flex items-center gap-3">
+                    <button onClick={handleNavigate} className="bg-blue-400 hover:bg-blue-700 text-white px-6 py-3 rounded flex justify-center items-center font-semibold mt-4 gap-3">
+                        Visit Store
+                        <ShoppingCartIcon className="h-5 w-5 text-white" />
+                    </button>
+                    <Link
+                        className="mt-3 font-semibold text-gray-900 hover:text-blue-700"
+                        to="/about"
+                    >
+                        Learn More
+                    </Link>
+                </div>
+            </div>
+            <div></div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import About from "./components/About.jsx";
+import Books from "./components/Books.jsx";
 import Home from "./components/Home.jsx";
 import "./index.css";
 
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home />,
+            },
+            {
+                path: "books",
+                element: <Books />,
             },
             {
                 path: "about",
